c4c3df10fcee7b9cfec90e157af0ac4f2f0d1367
roccopagliarulo <72402330+roccopagliarulo@users.noreply.github.com>
2021-01-24 15:57:34 +0100

    Update AccountDAOTest.java

--- bollicineWeb/src/test/java/Model/DAO/AccountDAOTest.java
+++ bollicineWeb/src/test/java/Model/DAO/AccountDAOTest.java
@@ -11,6 +11,17 @@ class AccountDAOTest {
 
     @Test
     void doSaveAcount() throws SQLException {
+        AccountDTO account = new AccountDTO(0,"Alfredo","Cornacchia","alfre@gmail.com","password","confermato","utente");
+        AccountDAO ac= new AccountDAO();
+
+        int id= ac.doSaveAcount(account);
+        System.out.println(id);
+
+        AccountDTO ac1= ac.doRetriveByEmail("alfre@gmail.com");
+        System.out.println(ac1.getEmail()+" "+ac1.getId());
+
+        assertEquals(id,ac1.getId());
+
     }
 
     @Test
